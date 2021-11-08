c57d395600a6f83bf5a58686f4d30dae17529108
rithiksinha <rithiksinha2000@gmail.com>
2021-11-08 14:15:44 +0530

    Initial Commit

---
+++ httpsrequest/httpsrequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  httpsrequestApp.swift
+//  httpsrequest
+//
+//  Created by Ritik Sinha on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct httpsrequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
